c7116f4b725f60d0fcf2f23b2e4d4059d75fd7f2
rahathossenantor <md.rahathossenantor@gmail.com>
2025-10-26 13:05:02 +0600

    Cart implemented

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import moon from "../assets/icons/moon.svg";
 import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import Cart from "../components/Cart";
 
 export default function Header() {
+    const [showCart, setShowCart] = useState(false);
+
+    function handleCartOpen() {
+        setShowCart(true);
+    }
+
     return (
         <header>
+            {showCart && <Cart onClose={() => setShowCart(false)} />}
             <nav className="container flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
                     <img src={logo} width="139" height="26" alt="logo" />
@@ -37,6 +46,7 @@ export default function Header() {
                         <a
                             className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                             href="#"
+                            onClick={handleCartOpen}
                         >
                             <img
                                 src={shoppingCart}
